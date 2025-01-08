3e9ef286932be7f5a883606a63511f16a0382720
Parth2684 <bhosle6006@gmail.com>
2025-01-08 18:50:43 +0530

    initial commit

---
+++ backend/db/db.js
@@ -0,0 +1,37 @@
+require("dotenv").config({
+    path: "../.env"
+})
+
+const mongoose = require("mongoose")
+
+mongoose.connect(process.env.MONGO_URI);
+console.log("Connected to MongoDB");
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6    
+    },
+    firstName: {
+        type: String,
+        required: true
+    }
+    lastName: {
+        type: String,
+        required: true
+    }
+})
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = {
+    User
+}
---
+++ backend/index.js
@@ -0,0 +1,13 @@
+
+const express = require("express");
+const mainRouter = require("./routes/index")
+const app = express()
+const cors = require("cors")
+const port = 3000;
+app.use(cors())
+app.use(express.json())
+app.use("/api/v1", mainRouter)
+
+app.listen(port, ()=>{
+    console.log(`Your app is listening on port ${port}`)
+})
