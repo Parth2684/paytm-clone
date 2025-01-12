29478d6567d075379dbd88e1573556536b607378
Parth2684 <bhosle6006@gmail.com>
2025-01-13 00:09:28 +0530

    Completed basic functioning frontend of the paytm i dont think i would commit anything after this because a basic full stack working paytm clone is completed

--- backend/middlewares/middleware.js
+++ backend/middlewares/middleware.js
@@ -3,6 +3,7 @@ require("dotenv").config({
 })
 const jwt = require("jsonwebtoken")
 const JWT_SECRET = process.env.JWT_SECRET;
+console.log(JWT_SECRET)
 
 const authMiddleware = (req, res, next) => {
     const authHeader = req.headers.authorization;
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,13 +1,28 @@
+import { useState, useEffect } from "react";
 import { Appbar } from "../components/AppBar"
 import { Balance } from "../components/Balance"
 import { Users } from "../components/Users"
+import axios from "axios";
 
 
 export const Dashboard = () => {
+    const [balance, setBalance] = useState(0);
+    useEffect(() => {
+        const fetchBalance = async () => {
+            const response = await axios.get("http://localhost:3000/api/v1/account/balance",{
+                headers: {
+                    Authorization: "Bearer " + localStorage.getItem("token")
+                }
+            })
+            setBalance(response.data.balance)
+        }
+        fetchBalance();
+    },[])
+    
     return <div>
         <Appbar />
         <div className="m-8">
-            <Balance value={"10000"} />
+            <Balance value={balance} />
             <Users />
         </div>
     </div>
